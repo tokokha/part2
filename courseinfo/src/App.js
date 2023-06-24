8c5053767463e167ab2dabe632ff25e365af1b8d
TokoNew <tornikekhabeishvili07@gmail.com>
2023-06-24 09:16:25 +0400

    Completed 2.1

---
+++ courseinfo/src/App.js
@@ -0,0 +1,49 @@
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Part = ({ info }) => 
+  
+  <p>
+    {info.name} {info.exercises}
+  </p>
+
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      {course.parts.map(part => 
+        <Part info={part} key={part.id} />
+      )}
+    </div>
+  )
+}
+
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
